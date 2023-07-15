818447fdb28e740389a5cee57109ec76235d9f8e
blessingmwiti <blessingmwiti@gmail.com>
2023-07-15 11:11:31 +0300

    damn

--- request.php
+++ request.php
@@ -1,15 +1,58 @@
 <?php
 //Mpesa variables initialization
-$consumer_key = '';
-$consumer_secret = '';
-$business_code = '';
-$Passkey = '';
-$Transaction_type = '';
-$Token_URL = '';
-$phone_number = $_POST[''];
-$OnlinePayment = '';
-$amount = $_POST[''];
+if(isset($_POST['submit'])){
+$consumer_key = 'Lxq4IWKR0E0Gt238SOMrcaD08otBmSAy';
+$consumer_secret = 'kTNPlprysfrIhoFK';
+$business_code = 174379;
+$Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
+$Transaction_type = 'CustomerPayBillOnline';
+$Token_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+$phone_number = $_POST['phoneNo'];
+$OnlinePayment = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+$amount = $_POST['amount'];
 $callBackURL = '';
 $Time_stamp = date("Ymdhis");
 $password = base64_encode($business_code . $Passkey. $Time_stamp);
+
+$curl_Tranfer = curl_init();
+curl_setopt($curl_Tranfer, CURLOPT_URL, $Token_URL);
+$credentials = base64_encode($consumer_key . ':' . $consumer_secret);
+curl_setopt($curl_Tranfer, CURLOPT_HTTPHEADER, array('Authorization: Basic ' . $credentials));
+curl_setopt($curl_Tranfer, CURLOPT_HEADER, false);
+curl_setopt($curl_Tranfer, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl_Tranfer, CURLOPT_SSL_VERIFYPEER, false);
+$curl_Tranfer_response = curl_exec($curl_Tranfer);
+
+$token = json_decode($curl_Tranfer_response)->access_token;
+
+$curl_Tranfer2 = curl_init();
+curl_setopt($curl_Tranfer2, CURLOPT_URL, $OnlinePayment);
+curl_setopt($curl_Tranfer2, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer ' . $token));
+
+$curl_Tranfer2_post_data = [
+    'BusinessShortCode' => $business_code,
+    'Password' => $password,
+    'Timestamp' =>$Time_stamp,
+    'TransactionType' =>$Transaction_type,
+    'Amount' => $amount,
+    'PartyA' => $phone_number,
+    'PartyB' => $business_code,
+    'PhoneNumber' => $phone_number,
+    'CallBackURL' => $callBackURL,
+    'AccountReference' => 'Ivonne',
+    'TransactionDesc' => 'Test',
+];
+
+$data2_string = json_encode($curl_Tranfer2_post_data);
+
+curl_setopt($curl_Tranfer2, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl_Tranfer2, CURLOPT_POST, true);
+curl_setopt($curl_Tranfer2, CURLOPT_POSTFIELDS, $data2_string);
+curl_setopt($curl_Tranfer2, CURLOPT_HEADER, false);
+curl_setopt($curl_Tranfer2, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($curl_Tranfer2, CURLOPT_SSL_VERIFYHOST, 0);
+$curl_Tranfer2_response = json_decode(curl_exec($curl_Tranfer2));
+
+echo json_encode($curl_Tranfer2_response, JSON_PRETTY_PRINT);
+}
 ?>
